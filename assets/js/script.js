e29b92f4c58adc1dfec40054e499611f82c278fc
paul88vera <paul88vera@gmail.com>
2021-12-15 19:55:16 -0600

    incomplete fetch/ finish tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
-// http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}
 
-// lat: 29.424349
-// lon: -98.491142
-// units: imperial or metric
+var getCityInfo = function(cityName){  
+  var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=def04aeb0bc1cff2cbceffb86658aa7d";
 
-//http://api.openweathermap.org/data/2.5/onecall?lat=29.424349&lon=-98.491142&units=imperial
-
-var apiUrl = "api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335";
-
-fetch(apiUrl) {
-  
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data){
+        displayWeather(data);
+        
+        if (response.headers.get('Link')) {
+          displayWarning(cityName);
+        }
+        
+      })
+    } else {
+      alert("There was an issue with your request");
+    }
+  });
 };
