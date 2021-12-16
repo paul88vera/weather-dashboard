19fde3234d003dbdfdd6d4ebdb258b422581d45f
paul88vera <paul88vera@gmail.com>
2021-12-15 18:37:36 -0600

    style complete

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={units}
+
+// lat: 29.424349
+// lon: -98.491142
+// units: imperial or metric
+
+//http://api.openweathermap.org/data/2.5/onecall?lat=29.424349&lon=-98.491142&units=imperial
+
+var apiUrl = "api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335";
+
+fetch(apiUrl) {
+  
+};
