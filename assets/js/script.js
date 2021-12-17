d21df9f443572bc1f69ff084bf57e8112ab3966a
paul88vera <paul88vera@gmail.com>
2021-12-17 15:26:06 -0600

    api works/ not complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,47 @@
 
-var getCityInfo = function(cityName){  
-  var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=def04aeb0bc1cff2cbceffb86658aa7d";
+var searchBtn = document.querySelector('#searchBtn');
+var name = document.querySelector('.name');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+var icon = document.querySelector('.icon');
 
-  fetch(apiUrl).then(function(response) {
-    if (response.ok) {
-      response.json().then(function(data){
-        displayWeather(data);
-        
-        if (response.headers.get('Link')) {
-          displayWarning(cityName);
-        }
-        
-      })
-    } else {
-      alert("There was an issue with your request");
-    }
-  });
-};
+
+
+var getCityInfo = function(city){
+  var city = document.querySelector('input').value;
+  
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&appid=def04aeb0bc1cff2cbceffb86658aa7d`;
+
+    fetch(apiUrl)
+    .then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          var nameValue = data.city.name;
+          // var dataValue = data.list[0].
+          var tempValue = data.list[0].main.temp;
+          var windValue = data.list[0].wind.speed;
+          var humidityValue = data.list[0].main.humidity;
+          // var iconValue = data.city.list[0].weather[0].icon;
+
+          name.textContent = "City Name: " + nameValue + " "  ;
+          temp.textContent = "Temp: " + tempValue;
+          wind.innerHTML = "Wind: " + windValue + "mph";
+          humidity.innerHTML = "Humidity: " + humidityValue + "%";
+          // icon.innerHTML = iconValue;
+          console.log(nameValue);
+          console.log(tempValue);
+          console.log(windValue);
+
+        })
+      } else {
+        alert('error');
+      }
+    });
+  };
+
+$('.searchBtn').click(function() {
+  console.log('click');
+  
+  getCityInfo();
+});
